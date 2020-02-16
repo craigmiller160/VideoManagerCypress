cad0115b305e2ea639392404ba5adc9852cf62f6
Craig Miller <craigmiller160@gmail.com>
2020-02-16 11:45:31 -0500

    Refactored envConstants to use commonjs modules

--- cypress/util/envConstants.js
+++ cypress/util/envConstants.js
@@ -1,10 +1,13 @@
-export const HOST_URL = 'host_url';
-export const ADMIN_USERNAME = 'admin_username';
-export const ADMIN_PASSWORD = 'admin_password';
-export const PG_USERNAME = 'pg_username';
-export const PG_PASSWORD = 'pg_password';
-export const PG_DB = 'vm_qa';
-export const PG_DROP_SCRIPT_PATH = 'pg_drop_script_path';
-export const PG_CREATE_SCRIPT_PATH = 'pg_create_script_path';
-export const PG_HOST = "pg_host";
-export const PG_PORT = "pg_port";
+
+module.exports = {
+    HOST_URL: 'host_url',
+    ADMIN_USERNAME: 'admin_username',
+    ADMIN_PASSWORD: 'admin_password',
+    PG_USERNAME: 'pg_username',
+    PG_PASSWORD: 'pg_password',
+    PG_DB: 'vm_qa',
+    PG_DROP_SCRIPT_PATH: 'pg_drop_script_path',
+    PG_CREATE_SCRIPT_PATH: 'pg_create_script_path',
+    PG_HOST: 'pg_host',
+    PG_PORT: 'pg_port'
+};
