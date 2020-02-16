ebe4c1aa81290d033cb94daa80269bcf1d1f99fb
Craig Miller <craigmiller160@gmail.com>
2020-02-15 22:58:23 -0500

    Configuring some environment settings for the test

--- cypress/integration/LoginPage.Spec.js
+++ cypress/integration/LoginPage.Spec.js
@@ -1,4 +1,10 @@
+import { HOST_URL } from '../util/envConstants';
+
 describe('Login Page', () => {
+    beforeEach(() => {
+        cy.visit(Cypress.env(HOST_URL));
+    });
+
     it('shows required validation warning for both input fields', () => {
         throw new Error();
     });
