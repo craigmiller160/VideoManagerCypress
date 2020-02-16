b8ce72a00fb3f9d746b32f2a7d1a71423c132cb9
Craig Miller <craigmiller160@gmail.com>
2020-02-16 08:38:28 -0500

    Added more properties

--- cypress/util/envConstants.js
+++ cypress/util/envConstants.js
@@ -4,3 +4,5 @@ export const ADMIN_PASSWORD = 'admin_password';
 export const PG_USERNAME = 'pg_username';
 export const PG_PASSWORD = 'pg_password';
 export const PG_DB = 'vm_qa';
+export const PG_DROP_SCRIPT_PATH = 'pg_drop_script_path';
+export const PG_CREATE_SCRIPT_PATH = 'pg_create_script_path';
