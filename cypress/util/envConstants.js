f0136064b397022e73ea94c925abb7a9390c443a
Craig Miller <craigmiller160@gmail.com>
2020-02-16 08:35:54 -0500

    Added more env constants

--- cypress/util/envConstants.js
+++ cypress/util/envConstants.js
@@ -1,1 +1,6 @@
 export const HOST_URL = 'host_url';
+export const ADMIN_USERNAME = 'admin_username';
+export const ADMIN_PASSWORD = 'admin_password';
+export const PG_USERNAME = 'pg_username';
+export const PG_PASSWORD = 'pg_password';
+export const PG_DB = 'vm_qa';
