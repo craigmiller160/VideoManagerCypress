910d0050e33810786e26b2803c68296620a4d494
Craig Miller <craigmiller160@gmail.com>
2020-02-15 22:55:32 -0500

    Stubbed out scenarios for Login Page

---
+++ cypress/integration/LoginPage.Spec.js
@@ -0,0 +1,17 @@
+describe('Login Page', () => {
+    it('shows required validation warning for both input fields', () => {
+        throw new Error();
+    });
+
+    it('shows email validation warning for username field', () => {
+        throw new Error();
+    });
+
+    it('logs into app', () => {
+        throw new Error();
+    });
+
+    it('rejects invalid credentials', () => {
+        throw new Error();
+    });
+});
