532d3a99a84a337d3a68f2236d862593304106a8
Craig Miller <craigmiller160@gmail.com>
2020-02-15 23:19:29 -0500

    Implemented some early, basic cypress tests

--- cypress/integration/LoginPage.Spec.js
+++ cypress/integration/LoginPage.Spec.js
@@ -1,20 +1,35 @@
 import { HOST_URL } from '../util/envConstants';
 
-const isOnLoginPage = () => {
-    cy.get('#login-title').contains('Login');
-};
-
 describe('Login Page', () => {
     beforeEach(() => {
         cy.visit(Cypress.env(HOST_URL));
+        cy.get('#login-title')
+            .should('have.text', 'Login');
     });
 
     it('shows required validation warning for both input fields', () => {
-        isOnLoginPage();
+        cy.get('#username-field-input')
+            .click();
+        cy.get('body')
+            .click();
+        cy.get('#username-field > span.text-danger')
+            .should('have.text', 'Required');
+
+        cy.get('#password-field-input')
+            .click();
+        cy.get('body')
+            .click();
+        cy.get('#password-field > span.text-danger')
+            .should('have.text', 'Required');
     });
 
     it('shows email validation warning for username field', () => {
-        throw new Error();
+        cy.get('#username-field-input')
+            .type('abc');
+        cy.get('body')
+            .click();
+        cy.get('#username-field > span.text-danger')
+            .should('have.text', 'Must be valid email');
     });
 
     it('logs into app', () => {
