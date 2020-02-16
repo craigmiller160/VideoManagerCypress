75c49a8d904f272b3a4106fd898d7001a09265f5
Craig Miller <craigmiller160@gmail.com>
2020-02-16 07:41:34 -0500

    Stubbed out scan spec

---
+++ cypress/integration/Scan.Spec.js
@@ -0,0 +1,10 @@
+describe('Scan Page', () => {
+    it('cannot scan without scan role', () => {
+        throw new Error();
+    });
+
+    it('runs scan and loads files', () => {
+        // TODO somehow need to add a new file here
+        throw new Error();
+    });
+});
--- cypress/integration/test.js
+++
@@ -1,6 +0,0 @@
-describe('Test spec', () => {
-	it('testing env', () => {
-		const url = Cypress.env('CYPRESS_host_url');
-		cy.visit(url);
-	});
-});
