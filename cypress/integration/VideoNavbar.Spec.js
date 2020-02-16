4d36b8f053bebf23d75a1c674e9b76893b237e47
Craig Miller <craigmiller160@gmail.com>
2020-02-16 07:40:16 -0500

    Set up Navbar spec template

---
+++ cypress/integration/VideoNavbar.Spec.js
@@ -0,0 +1,17 @@
+describe('Navbar appearance and actions by user role', () => {
+    it('user has admin role', () => {
+        throw new Error();
+    });
+
+    it('user has edit role', () => {
+        throw new Error();
+    });
+
+    it('user has scan role', () => {
+        throw new Error();
+    });
+
+    it('user has no roles', () => {
+        throw new Error();
+    });
+});
