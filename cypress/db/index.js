51137a36c6890a9e8ca8bf97e3c5c5abc2a2949a
Craig Miller <craigmiller160@gmail.com>
2020-02-16 12:15:32 -0500

    Added hashed password

--- cypress/db/index.js
+++ cypress/db/index.js
@@ -2,6 +2,8 @@ const getDdlScripts = require('./getDdlScripts');
 const getClient = require('./getClient');
 const createSchema = require('./createSchema');
 
+// $2a$10$ytpfmAxR8lh9KwnWtkiinOYWn2779iJE3rE6BWLzHzwEh9Q9hJGv2
+
 class PG {
     constructor() { }
 
