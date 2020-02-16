fc25193947a5f4c6b3eba1b240cc0afe40ea64d5
Craig Miller <craigmiller160@gmail.com>
2020-02-15 23:32:50 -0500

    Updated more tests

--- cypress/integration/LoginPage.Spec.js
+++ cypress/integration/LoginPage.Spec.js
@@ -1,10 +1,14 @@
 import { HOST_URL } from '../util/envConstants';
 
+// TODO add validation that the login button stays disabled until valid input is entered.
+
 describe('Login Page', () => {
     beforeEach(() => {
         cy.visit(Cypress.env(HOST_URL));
         cy.get('#login-title')
             .should('have.text', 'Login');
+        cy.url()
+            .should('include', '/login');
     });
 
     it('shows required validation warning for both input fields', () => {
@@ -38,11 +42,20 @@ describe('Login Page', () => {
         cy.get('#password-field-input')
             .type(Cypress.env('admin_password'));
         cy.get('#login-btn').click();
+
         cy.get('#home-title')
             .should('have.text', 'Welcome to VideoManager');
+        cy.url()
+            .should('not.include', '/login');
     });
 
     it('rejects invalid credentials', () => {
-        throw new Error();
+        cy.get('#username-field-input')
+            .type('abc@gmail.com');
+        cy.get('#password-field-input')
+            .type('abc');
+        cy.get('#login-btn').click();
+
+        // TODO need to add custom attributes to the element to detect the dynamic classes being added
     });
 });
