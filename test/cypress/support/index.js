6d59b1591e9e44e8bd54c500225a067499f57ce3
Craig Miller <craigmiller160@gmail.com>
2020-02-17 17:18:33 -0500

    Added them to global start/stop methods

--- test/cypress/support/index.js
+++ test/cypress/support/index.js
@@ -2,8 +2,10 @@ require('./commands');
 
 before(() => {
     cy.task('initDb', { env: Cypress.env() });
+    cy.task('initFiles');
 });
 
 after(() => {
     cy.task('closeDb');
+    cy.task('cleanFiles');
 });
