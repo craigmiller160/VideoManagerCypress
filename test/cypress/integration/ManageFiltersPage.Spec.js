09e34ddd7ac6e3ff4b3bb8402c0b7c88dffd7aeb
Craig Miller <craigmiller160@gmail.com>
2020-02-23 12:03:23 -0500

    Implemented test to verify items on page

--- test/cypress/integration/ManageFiltersPage.Spec.js
+++ test/cypress/integration/ManageFiltersPage.Spec.js
@@ -1,13 +1,59 @@
+const { HOST_URL } = require('../../util/envConstants');
+const { users: { standard, edit }, password } = require('../../util/users');
+
 describe('Manage Filters Page', () => {
     beforeEach(() => {
         cy.visit(Cypress.env(HOST_URL));
     });
 
     it('cannot access without edit role', () => {
-        throw new Error();
+        cy.login(standard.userName, password);
+        cy.get('#manageFiltersLink_navLink')
+            .should('not.exist');
     });
 
     describe('has access', () => {
+        beforeEach(() => {
+            cy.login(edit.userName, password);
+        });
+
+        it('page has expected elements', () => {
+            cy.get('#manageFiltersLink_navLink')
+                .click();
+            cy.get('#manage-filters-title')
+                .should('have.text', 'Manage Filters');
+
+            // Categories
+            cy.get('#category-filters')
+                .should('exist');
+            cy.get('#category-filters-title')
+                .should('have.text', 'Categories');
+            cy.get('#category-filters-list')
+                .should('exist');
+            cy.get('#category-filters-add-btn')
+                .should('have.text', '+');
+
+            // Series
+            cy.get('#series-filters')
+                .should('exist');
+            cy.get('#series-filters-title')
+                .should('have.text', 'Series');
+            cy.get('#series-filters-list')
+                .should('exist');
+            cy.get('#series-filters-add-btn')
+                .should('have.text', '+');
+
+            // Stars
+            cy.get('#star-filters')
+                .should('exist');
+            cy.get('#star-filters-title')
+                .should('have.text', 'Stars');
+            cy.get('#star-filters-list')
+                .should('exist');
+            cy.get('#star-filters-add-btn')
+                .should('have.text', '+');
+        });
+
         it('add new category', () => {
             throw new Error();
         });
