293cf43831acab9d46b357d90c100878e4eb97a4
Craig Miller <craigmiller160@gmail.com>
2020-02-23 14:23:16 -0500

    Migrated all selectors in VideoNavbar.Spec to the new selector constants files

--- test/cypress/selectors/navbar.js
+++ test/cypress/selectors/navbar.js
@@ -1,4 +1,11 @@
 module.exports = {
     SCAN_DIR_LINK: '#scanDirectoryLink_text',
-    VIDEO_LIST_LINK: '#videoListLink_navLink'
+    VIDEO_LIST_LINK: '#videoListLink_navLink',
+    HOME_LINK: '#home-link',
+    USER_MANAGEMENT_LINK: '#userManagementLink_navLink',
+    MANAGE_FILTERS_LINK: '#manageFiltersLink_navLink',
+    SETTINGS_LINK: '#settingsLink_navLink',
+    PROFILE_TOGGLE_BTN: '#vm-navbar-dropdown-toggle',
+    USER_PROFILE_LINK: '#userProfileLink_navLink',
+    LOGOUT_LINK: '#logoutLink_text'
 };
