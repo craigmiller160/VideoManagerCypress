3616abf2f00365e553f94865a3a5854d48add924
Craig Miller <craigmiller160@gmail.com>
2020-02-23 15:45:15 -0500

    Expanded the capabilities of insertVideoFiles

--- test/db/insertVideoFiles.js
+++ test/db/insertVideoFiles.js
@@ -1,23 +1,41 @@
 
-const INSERT_STMT = 'INSERT INTO video_files (description, display_name, file_name, file_added, last_modified, last_scan_timestamp, last_viewed, view_count, active) ' +
+const INSERT_FILE_STMT = 'INSERT INTO video_files (description, display_name, file_name, file_added, last_modified, last_scan_timestamp, last_viewed, view_count, active) ' +
     'VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)';
 
-const insertVideoFiles = async (client, { files }) => {
+const INSERT_CATEGORY_STMT = 'INSERT INTO categories (category_name) VALUES ($1)';
+
+const insertVideoFiles = async (client, { files, categories }) => {
     console.log('Inserting video files');
-    const promises = files.map(async (file) => {
-        const args = [
-            file.description || '',
-            file.displayName || '',
-            file.fileName || '',
-            file.fileAdded || '2020-01-01',
-            file.lastModified || '2020-01-01',
-            file.lastScanTimestamp || '2020-01-01',
-            file.lastViewed || '2020-01-01',
-            file.viewCount || 0,
-            file.active || true
-        ];
-        await client.query(INSERT_STMT, args);
-    });
+    let filePromises = [];
+    let categoryPromises = [];
+
+    if (categories) {
+        categoryPromises = categories.map((category) => {
+            const args = [
+                category.categoryName || ''
+            ];
+            return client.query(INSERT_CATEGORY_STMT, args);
+        });
+    }
+
+    if (files) {
+        filePromises = files.map((file) => {
+            const args = [
+                file.description || '',
+                file.displayName || '',
+                file.fileName || '',
+                file.fileAdded || '2020-01-01',
+                file.lastModified || '2020-01-01',
+                file.lastScanTimestamp || '2020-01-01',
+                file.lastViewed || '2020-01-01',
+                file.viewCount || 0,
+                file.active || true
+            ];
+            return client.query(INSERT_FILE_STMT, args);
+        });
+    }
+
+    const promises = filePromises.concat(categoryPromises);
     await Promise.all(promises);
 };
 
