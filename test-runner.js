b9e1e328278ee08954a31ced769a0f7bccef8fc4
Craig Miller <craigmiller160@gmail.com>
2020-02-23 09:00:43 -0500

    Got the basics of the mocha-awesome report working

--- test-runner.js
+++ test-runner.js
@@ -1,10 +1,14 @@
-const cypress = require('cypress')
-const yargs = require('yargs')
-const { merge } = require('mochawesome-merge')
-const marge = require('mochawesome-report-generator')
-const rm = require('rimraf')
-const cypressConfig = require('./cypress')
-const ls = require('ls')
+const cypress = require('cypress');
+const yargs = require('yargs');
+const { merge } = require('mochawesome-merge');
+const marge = require('mochawesome-report-generator');
+const rm = require('rimraf');
+const cypressConfig = require('./cypress');
+const ls = require('ls');
+
+console.log(cypressConfig.reporterOptions); // TODO delete this
+
+// TODO probably don't need this
 const argv = yargs.options({
     'browser': {
         alias: 'b',
@@ -18,37 +22,41 @@ const argv = yargs.options({
         default: 'cypress/integration/*.spec.js'
     }
 }).help()
-    .argv
+    .argv;
 
-const reportDir = cypressConfig.reporterOptions.reportDir
-const reportFiles = `${reportDir}/*.json`
+const reportDir = cypressConfig.reporterOptions.reportDir;
+const reportFiles = `${reportDir}/*.json`;
 // list all of existing report files
-ls(reportFiles, { recurse: true }, file => console.log(`removing ${file.full}`))
+ls(reportFiles, { recurse: true }, file => console.log(`removing ${file.full}`));
 
 // delete all existing report files
 rm(reportFiles, (error) => {
     if (error) {
-        console.error(`Error while removing existing report files: ${error}`)
+        console.error(`Error while removing existing report files: ${error}`);
         process.exit(1)
     }
     console.log('Removing all existing report files successfully!')
-})
-
-cypress.run({
-    browser: argv.browser,
-    spec: argv.spec
-}).then((results) => {
-    const reporterOptions = {
-        reportDir: results.config.reporterOptions.reportDir,
-    }
-    generateReport(reporterOptions)
-}).catch((error) => {
-    console.error('errors: ', error)
-    process.exit(1)
-})
+});
 
 function generateReport(options) {
     return merge(options).then((report) => {
         marge.create(report, options)
-    })
+    });
 }
+
+cypress.run({
+    // browser: argv.browser,
+    spec: 'test/cypress/integration/working/**/*'
+}).then((results) => {
+    // TODO if no tests are found, this will blow up
+    const reporterOptions = {
+        reportDir: results.config.reporterOptions.reportDir,
+    };
+    generateReport({
+        files: ['./reports/*.json']
+    })
+}).catch((error) => {
+    console.error('errors: ', error)
+    process.exit(1)
+});
+
