9c6419278ebd07fd94e13c192034f0dbfb574a0a
Craig Miller <craigmiller160@gmail.com>
2020-02-23 13:12:16 -0500

    Began refactoring the login page selectors

--- test/cypress/integration/working/LoginPage.Spec.js
+++ test/cypress/integration/working/LoginPage.Spec.js
@@ -1,33 +1,41 @@
 const { HOST_URL } = require('../../../util/envConstants');
 const { users: { admin }, password } = require('../../../util/users');
+const {
+    LOGIN_TITLE,
+    LOGIN_BTN,
+    USERNAME_FIELD,
+    USERNAME_FIELD_ERROR,
+    PASSWORD_FIELD,
+    PASSWORD_FIELD_ERROR
+} = require('../../selectors/login');
 
 describe('Login Page', () => {
     beforeEach(() => {
         cy.visit(Cypress.env(HOST_URL));
-        cy.get('#login-title')
+        cy.get(LOGIN_TITLE)
             .should('have.text', 'Login');
         cy.url()
             .should('include', '/login');
-        cy.get('#login-btn')
+        cy.get(LOGIN_BTN)
             .should('be.disabled');
     });
 
     it('shows required validation warning for both input fields', () => {
-        cy.get('#username-field-input')
+        cy.get(USERNAME_FIELD)
             .click();
         cy.get('body')
             .click();
-        cy.get('#username-field > span.text-danger')
+        cy.get(USERNAME_FIELD_ERROR)
             .should('have.text', 'Required');
 
-        cy.get('#password-field-input')
+        cy.get(PASSWORD_FIELD)
             .click();
         cy.get('body')
             .click();
-        cy.get('#password-field > span.text-danger')
+        cy.get(PASSWORD_FIELD_ERROR)
             .should('have.text', 'Required');
 
-        cy.get('#login-btn')
+        cy.get(LOGIN_BTN)
             .should('be.disabled');
     });
 
@@ -44,14 +52,14 @@ describe('Login Page', () => {
     });
 
     it('logs into app', () => {
-        cy.get('#username-field-input')
+        cy.get(USERNAME_FIELD)
             .type(admin.userName);
-        cy.get('#password-field-input')
+        cy.get(PASSWORD_FIELD)
             .type(password);
 
-        cy.get('#login-btn')
+        cy.get(LOGIN_BTN)
             .should('not.be.disabled');
-        cy.get('#login-btn').click();
+        cy.get(LOGIN_BTN).click();
 
         cy.get('#home-title')
             .should('have.text', 'Welcome to VideoManager');
