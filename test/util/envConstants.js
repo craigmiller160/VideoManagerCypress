4989351013cd391bd82b330d4c4ef96a3502ce26
Craig Miller <craigmiller160@gmail.com>
2020-04-12 12:04:29 -0400

    Cleaned up old env values

--- test/util/envConstants.js
+++ test/util/envConstants.js
@@ -1,12 +1,7 @@
 
-// TODO remove the postgres ones after we migrate to cloud config server
 module.exports = {
     HOST_URL: 'host_url',
-    PG_USERNAME: 'pg_username',
-    PG_PASSWORD: 'pg_password',
     PG_DB: 'pg_db',
     VM_SERVER_PATH: 'vm_server_path',
-    PG_HOST: 'pg_host',
-    PG_PORT: 'pg_port',
     CLOUD_CONFIG_HOST: 'cloud_config_host'
 };
